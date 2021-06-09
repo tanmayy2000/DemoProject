8cf5bbedc57272c07f8beae2052875376928fb9f
tanmayy2000 <kumawat.tanmay@cloudanalogy.com>
2021-06-09 11:45:37 +0530

    add heading

---
+++ force-app/main/default/lwc/companyEmployeesRecord/companyEmployeesRecord.js
@@ -0,0 +1,108 @@
+import {LightningElement, track, wire} from 'lwc';
+import getEmployees from '@salesforce/apex/CompanyController.getEmployees';
+import {refreshApex} from '@salesforce/apex';
+const columns = [
+    { label: 'Name', fieldName: 'Name' }, 
+    { label: 'Id', fieldName: 'EmployeeId__c' },
+    { label: 'Address', fieldName: 'Address__c', type: 'text'}, 
+    { label: 'Company', fieldName: 'Company__c', type: 'text' }, 
+    { label: 'Phone', fieldName: 'Phone__c', type: 'phone'}, 
+    { label: 'Email', fieldName: 'Email__c', type: 'email' }, 
+];
+export default class CompanyEmployeesRecord extends LightningElement { 
+    @track data;
+    @track value;
+    @track columns = columns;
+    @track record = [];
+    @track searchKey = '';
+    @track page=1;
+    @track items=[];
+    @track data =[];
+    @track startingRecord=1;
+    @track endingRecord = 0;
+    @track pageSize = 5;
+    @track totalRecountCount=0;
+    @track totalPage=0;
+    selectedRecords = [];
+    result;
+    refreshTable;
+    error;
+
+    get options() {
+        return [
+            { label: '5', value: '5' },
+            { label: '10', value: '10' },
+            { label: '15', value: '15' },
+            { label: '20', value: '20' }
+
+        ];
+    }
+    @wire(getEmployees,{searchKey: '$searchKey'})
+    wiredEmployees({error,data}) {
+        this.refreshTable = data;
+        if (data) {
+            this.items = data;
+            this.totalRecountCount = data.length; 
+            this.totalPage = Math.ceil(this.totalRecountCount / this.pageSize); 
+            
+            this.data = this.items.slice(0,this.pageSize); 
+            this.endingRecord =this.pageSize;
+            this.columns = columns;
+            this.error = undefined;
+
+        } else if (error) {
+            this.error = error;
+            this.data = undefined;
+        }
+    }
+    //clicking on previous button this method will be called
+    previousHandler() {
+        if (this.page > 1) {
+            this.page = this.page - 1; //decrease page by 1
+            this.displayRecordPerPage(this.page);
+        }
+    }
+
+    //clicking on next button this method will be called
+    nextHandler() {
+        if((this.page<this.totalPage) && this.page !== this.totalPage){
+            this.page = this.page + 1; //increase page by 1
+            this.displayRecordPerPage(this.page);            
+        }             
+    }
+
+    //this method displays records page by page
+    displayRecordPerPage(page){
+
+        this.startingRecord = ((page -1) * this.pageSize) ;
+        this.endingRecord = (this.pageSize * page);
+
+        this.endingRecord = (this.endingRecord > this.totalRecountCount) 
+                            ? this.totalRecountCount : this.endingRecord; 
+
+        this.data = this.items.slice(this.startingRecord, this.endingRecord);
+
+        this.startingRecord = this.startingRecord + 1;
+    }    
+    handleKeyChange( event ) {
+        this.searchKey = event.target.value;
+        this.endingRecord=this.totalRecountCount;
+        return refreshApex(this.result);
+    }
+    
+    handleChangePageSize(event) {
+        this.pageSize = event.target.value;
+        this.totalPage = Math.ceil(this.totalRecountCount / this.pageSize);
+        this.page = 1;
+        this.displayRecordPerPage(this.page);
+    }
+    handleFirstPage(){
+        this.page = 1;
+        this.displayRecordPerPage(this.page);
+    }
+    handleLastPage()
+    {
+        this.page = this.totalPage;
+        this.displayRecordPerPage(this.page);
+    }
+}
---
+++ force-app/main/default/lwc/customDataTable/customDataTable.js
@@ -0,0 +1,60 @@
+import { LightningElement, track, wire } from 'lwc';
+
+// importing Apex Class
+import retriveCons from '@salesforce/apex/ContactController.getContacts';
+
+export default class CustomDataTable extends LightningElement {
+    // reactive variables
+    @track data = [];
+    @track error;
+    @track bShowModal = false;
+    @track selectedCons;
+
+    // opening the modal
+    openModal() { this.bShowModal = true; }
+    // closeing the modal
+    closeModal() { this.bShowModal = false;}
+
+    // Getting Contacts using Wire Service
+    @wire(retriveCons)
+    contacts(result) {
+        if (result.data) {
+            this.data = result.data;
+            this.error = undefined;
+
+        } else if (result.error) {
+            this.error = result.error;
+            this.data = undefined;
+        }
+    }
+
+    // Select the all rows
+    allSelected(event) {
+        let selectedRows = this.template.querySelectorAll('lightning-input');
+        
+        for(let i = 0; i < selectedRows.length; i++) {
+            if(selectedRows[i].type === 'checkbox') {
+                selectedRows[i].checked = event.target.checked;
+            }
+        }
+    }
+
+    showContacts() {
+        this.bShowModal = true;
+
+        this.selectedCons = [];
+
+        let selectedRows = this.template.querySelectorAll('lightning-input');
+
+        // based on selected row getting values of the contact
+        for(let i = 0; i < selectedRows.length; i++) {
+            if(selectedRows[i].checked && selectedRows[i].type === 'checkbox') {
+                this.selectedCons.push({
+                    Name: selectedRows[i].value,
+                    Id: selectedRows[i].dataset.id
+                })
+            }
+        }
+    }
+
+}
